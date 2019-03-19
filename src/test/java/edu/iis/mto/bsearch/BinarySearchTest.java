f0a9c77710fce1c58cd7e26f3a6d53fdb0319ab0
Kovalyk.Volodymyr <211646@edu.p.lodz.pl>
2019-03-19 19:26:01 +0100

    test for one element

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -1,5 +1,6 @@
 package edu.iis.mto.bsearch;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,5 +9,15 @@ public class BinarySearchTest {
 
     @Test
     public void search() {
+    }
+
+    @Test
+    public void IfSqunsiHaveOnlyOneElement(){
+        int[] seq ={3};
+        int itemtoFind=3;
+        SearchResult searchResult =BinarySearch.search(itemtoFind,seq);
+        Assert.assertTrue(searchResult.isFound());
+        Assert.assertEquals(0,searchResult.getPosition());
+
     }
 }
